840fbbe98801e062bc2a1b86cd886584dbe01bd8
Sebastian Bazley <sebb@apache.org>
2015-07-18 23:56:25 +0000

    Add a test driver
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1691785 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scandist_test.py
@@ -0,0 +1,55 @@
+"""
+Simple test harness
+
+Reads output from svnpubsub/wacher.py (or similar)
+Input is parsed by scandist.processTargets()
+
+If a sufficient gap occurs between commits, then
+the parsed data is passed to scandist.processCommit()
+
+TODO: needs to check the output somehow
+
+"""
+
+import scandist
+import json
+import ast
+import datetime
+
+def processBatch():
+    for project in scandist.targets:
+        # show what is to be processed
+        print "Batch: <<",project,
+        committers = scandist.targets[project]
+        for committer in committers:
+            print committer,
+            for entry in committers[committer]:
+                print entry['id'],
+        print ">>"
+    scandist.processTargets()
+    
+if __name__ == "__main__":
+    prevdate=None
+    scandist.sendEmail = False
+    #scandist.trace = True
+    with open("scandist_test.dat") as data:
+        inCommit = False
+        for line in data:
+            if inCommit:
+                chunk += line
+            if line.startswith("COMMIT:"):
+                inCommit = True
+                chunk=""
+            elif line.endswith("}\n"):
+                inCommit = False
+                commit = ast.literal_eval(chunk)
+                rawDate = commit['date'][:19] # drop the unparseable part (unnecessary for diffs anyway)
+                date=datetime.datetime.strptime(rawDate,"%Y-%m-%d %H:%M:%S")# does not seem to support %z??
+                if not prevdate == None:
+                    diffSecs = (date - prevdate).seconds
+                    if diffSecs > 90:
+                        processBatch()
+                prevdate = date
+                scandist.processCommit(commit)
+
+    processBatch() # process any trailing data
