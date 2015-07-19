916fd94b27e9e678a3a6766f2b2fd8848c5da4c3
Sebastian Bazley <sebb@apache.org>
2015-07-19 00:06:44 +0000

    Ensure old backups are preseved
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1691786 13f79535-47bb-0310-9956-ffa450edef68

--- restart_scandist.sh
+++ restart_scandist.sh
@@ -2,5 +2,5 @@
 # TODO: this should really be integrated into system startup
 
 pkill -u root -f "scandist.py foreground"
-mv nohup.out nohup.out.$(date -Idate)
+mv --backup=numbered nohup.out nohup.out.$(date -Idate)
 nohup python -u scandist.py foreground &
