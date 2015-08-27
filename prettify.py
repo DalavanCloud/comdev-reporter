19b5602d3e19236e8ba9351a610154d29336516e
Sebastian Bazley <sebb@apache.org>
2015-08-27 08:49:58 +0000

    Improve handling of errors reading input file
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1698090 13f79535-47bb-0310-9956-ffa450edef68

--- prettify.py
+++ prettify.py
@@ -8,12 +8,12 @@ for arg in sys.argv[1:]:
         with open(arg, "r") as f:
             input = json.loads(f.read())
             f.close()
-    except:
+    except Exception as ex:
+        print(ex)
         pass
-
-    out = arg + ".out"
-    print("Writing " + out)
-    with open(out, "w") as f:
-        f.write(json.dumps(input, indent=1, sort_keys=True))
-        f.close()
-
+    else:
+        out = arg + ".out"
+        print("Writing " + out)
+        with open(out, "w") as f:
+            f.write(json.dumps(input, indent=1, sort_keys=True))
+            f.close()
