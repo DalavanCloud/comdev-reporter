082cd440ed0cb402554b44414441b3fcdfa820ef
Sebastian Bazley <sebb@apache.org>
2015-08-27 08:13:29 +0000

    Sorted output
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1698087 13f79535-47bb-0310-9956-ffa450edef68

--- data/parsepmcs.py
+++ data/parsepmcs.py
@@ -84,12 +84,12 @@ for project in pmcs:
     
 print("Writing pmcs.json")
 with open("pmcs.json", "w") as f:
-    f.write(json.dumps(pmcs, indent=1))
+    f.write(json.dumps(pmcs, sort_keys=True, indent=1))
     f.close()
 
 print("Writing projects.json")
 with open("projects.json", "w") as f:
-    f.write(json.dumps(projects, indent=1))
+    f.write(json.dumps(projects, sort_keys=True ,indent=1))
     f.close()
     
     
