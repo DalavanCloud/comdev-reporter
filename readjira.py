5c2c01a137a9c621372aef37b65454cdd2120cac
Sebastian Bazley <sebb@apache.org>
2015-09-11 17:54:06 +0000

    Extract parent directory as constant
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1702526 13f79535-47bb-0310-9956-ffa450edef68

--- readjira.py
+++ readjira.py
@@ -4,14 +4,16 @@ import re, os, json, urllib2, base64, time
 from os import listdir
 from os.path import isfile, join
 
-mypath = "/var/www/reporter.apache.org/data/JIRA"
+MYHOME = "/var/www/reporter.apache.org"
+mypath = "%s/data/JIRA" % MYHOME
+print("Scanning mypath=%s" % mypath)
 myfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
 
 jirapass = ""
-with open("/var/www/reporter.apache.org/data/jirapass.txt", "r") as f:
+with open("%s/data/jirapass.txt" % MYHOME, "r") as f:
     jirapass = f.read().strip()
     f.close()
-    
+
 def getJIRAS(project):
     refresh = True
     if refresh:
@@ -24,12 +26,12 @@ def getJIRAS(project):
             req = urllib2.Request("""https://issues.apache.org/jira/rest/api/2/search?jql=project%20=%20""" + project + """%20AND%20resolved%20%3E=%20-91d""")
             req.add_header("Authorization", "Basic %s" % base64string)
             rdata = json.loads(urllib2.urlopen(req).read())
-            with open("/var/www/reporter.apache.org/data/JIRA/%s.json" % project, "w") as f:
+            with open("%s/data/JIRA/%s.json" % (MYHOME, project), "w") as f:
                 f.write(json.dumps([cdata['total'], rdata['total'], project], indent=1))
                 f.close()
             return cdata['total'], rdata['total'], project
         except Exception as err:
-            with open("/var/www/reporter.apache.org/data/JIRA/%s.json" % project, "w") as f:
+            with open("%s/data/JIRA/%s.json" % (MYHOME, project), "w") as f:
                 f.write(json.dumps([0,0,None], indent=1))
                 f.close()
             return 0,0, None
@@ -40,4 +42,5 @@ for project in myfiles:
         print("Refreshing JIRA stats for " + jiraname)
         getJIRAS(jiraname)
         time.sleep(2)
-    
+
+print("Done")
