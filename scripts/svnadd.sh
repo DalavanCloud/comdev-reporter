2bb27e41e7b40224d78b79f124970504a0e1e0d8
Sebastian Bazley <sebb@apache.org>
2015-11-12 14:39:33 +0000

    Add svn add logging script
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1714057 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scripts/svnadd.sh
@@ -0,0 +1,43 @@
+# Script to run "svn add" and redirect standard output to /var/log/www-data
+# adds header and trailer to any output
+
+# Output log file is named after the SVN directory name plus the suffix _YYYY-MM
+# This ensures that at most one month's data is in each log file
+
+# Sample crontab entry:
+# 40 * * * *      cd /var/www/reporter.apache.org/scripts && ./svnadd.sh ../data/releases
+
+STARTED=$(date '+%Y-%m-%d %H:%M:%S')
+
+SVNDIR=${1?SVN directory}
+
+LOGDIR=/var/log/www-data
+
+BASE=$(basename $SVNDIR)
+
+YYMM=$(date '+%Y-%m')
+
+echo "exec  >>${LOGDIR}/${BASE}_${YYMM}.log"
+
+svn status $SVNDIR | awk '/^\? / {print $2}' | xargs -r svn add | \
+{
+    # Read one line first
+    IFS= read -r line && \
+    {
+        # add header
+        echo
+        echo '>>>'
+        echo "Starting  'svn add $SVNDIR' at $STARTED"
+        echo "$line"
+
+        # read the rest of the lines
+        while IFS= read -r line
+        do
+            echo "$line"
+        done
+        
+        # add trailer
+        echo "Completed 'svn add $SVNDIR' at $(date '+%Y-%m-%d %H:%M:%S')"
+        echo '<<<'
+    }
+}
