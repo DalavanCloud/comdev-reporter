8e14c1c6e44f166a8b8660b6dfd1906ffa010a3f
Sebastian Bazley <sebb@apache.org>
2016-02-07 01:03:13 +0000

    Update for getjson.py
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1728906 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/committee_info.py
+++ scripts/committee_info.py
@@ -65,120 +65,55 @@ def PMCnames():
 
     return namejson
 
-def cycles():
+def PMCsummary():
 
+    """
+        Returns output of the form:
+        {
+         "ace": {
+           "name": "Apache ACE",
+           "chair": "Chair 1",
+           "report": [
+             "February",
+             "May",
+             "August",
+             "November"
+             ]
+           },
+         "abdera": {
+           "name": "Apache Abdera",
+           "chair": "Chair 2",
+           "report: [...]
+           },
+         ...
+        }
+        Only includes actual PMCs
+        Returns 'webservices' rather than 'ws'
+    """
     committees = cidata['committees']
 
-    cycles={}
+    namejson={}
     for ctte in committees:
         c = committees[ctte]
         if not c['pmc']:
             continue
-        cycles[ctte] = c['report']
-        # Duplicate some entries for now so the code can find them (the existing json has the duplicates)
-        if ctte == 'ws': # Special processing
-            cycles['webservices'] = cycles[ctte]
-        if ctte == 'httpd': # Special processing
-            cycles['http server'] = cycles[ctte]
-    return cycles
+        name = 'Apache %s' % c['display_name']
+        if ctte == 'ws': ctte = 'webservices'
+        chair = 'Unknown'
+        chs = c['chair']
+        for ch in chs: # allow for multiple chairs
+            chair = chs[ch]['name']
+            break
+        namejson[ctte] = {
+                      'name': name,
+                      'report': c['report'],
+                      'chair': chair
+                      }
 
-"""
-Returns an array of entries of the form:
+    return namejson
 
-    "abdera": {
-      "fullname": "Apache Abdera",
-      "mail_list": "abdera",
-      "established": "2008-11",
-      "report": [
-        "February",
-        "May",
-        "August",
-        "November"
-      ],
-       "reporting": 2,
-      "chair": {
-        "nick": "antelder",
-        "name": "Ant Elder"
-        },
-      "pmc": true
-      },
-
-"""
-def committees():
-
-    committees = {}
-    cttes = cidata['committees']
-    for ent in cttes:
-        ctte = cttes[ent]
-        c = {}
-        for key in ctte:
-            # some keys need special processing
-            if key == 'display_name':
-                basename = ctte['display_name']
-                c['fullname'] = "Apache %s" % ('mod_perl' if basename == 'Perl' else basename)
-            elif key == 'chair':
-                c['chair'] = None
-                for ch in ctte['chair']:
-                    c['chair'] = {
-                    'nick': ch,
-                    'name': ctte['chair'][ch]['name']}
-            elif key == 'established':
-                value = ctte[key]
-                if value:
-                    value = "%s-%s" % (value[3:7], value[0:2]) # extract year and month
-                c[key] = value
-            elif key == 'report':
-                c[key] = ctte[key] # save original values
-                value = ctte[key]
-                if 'January' in value:
-                    c['reporting'] = 1
-                elif 'February' in value:
-                    c['reporting'] = 2
-                elif 'March' in value:
-                    c['reporting'] = 3
-                elif 'Every month' in value:
-                    c['reporting'] = 0
-            else:
-                c[key] = ctte[key]
-        committees[ent]=c
-    return committees
-
-def pmcdates():
-    dates = {}
-    
-    cttes = cidata['committees']
-    for ent in cttes:
-        ctte = cttes[ent]
-        if not ctte['pmc']:
-            continue
-        roster = ctte['roster']
-        est = ctte['established']
-        date = 0
-        if not est == None:
-            # convert mm/yyyy to date (drop any subsequent text)
-            try:
-                date = calendar.timegm(time.strptime(est[0:7], '%m/%Y'))
-            except Exception as e:
-                print("Date parse error for %s: %s %s" % (ent, est, e))
-                pass
-        dates[ent] = {'pmc': [est, date], 'roster': {} }
-        ids = {}
-        for id in roster:
-            rid = roster[id]
-            try:
-                date = calendar.timegm(time.strptime(rid['date'], '%Y-%m-%d'))
-            except:
-                date = 0
-            ids[id] = [rid['name'], date]
-        dates[ent]['roster'] = ids
-        # The 'CI' internal name for Web Services is 'ws' but reporter code originally used 'webservices'
-        if ent == 'ws':
-            dates['webservices'] = dates[ent]
-    return dates
 
 if __name__ == '__main__':
     import sys
     json.dump(PMCnames(), sys.stdout, indent=1, sort_keys=True)
-    json.dump(cycles(), sys.stdout, indent=1, sort_keys=True)
-    json.dump(pmcdates(), sys.stdout, indent=1, sort_keys=True)
-    json.dump(committees(), sys.stdout, indent=1, sort_keys=True)
+    json.dump(PMCsummary(), sys.stdout, indent=1, sort_keys=True)
