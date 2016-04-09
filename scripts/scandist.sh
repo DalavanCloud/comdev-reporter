5b7b41912fcf24ca3ab25c8195951efd24f58395
Sebastian Bazley <sebb@apache.org>
2016-04-09 22:50:10 +0000

    Prepare to move to scandist daemon
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1738392 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scripts/scandist.sh
@@ -0,0 +1,23 @@
+SCRIPT=scandist.py
+
+LOGDIR=/var/log/www-data
+
+BASE=$(basename $SCRIPT .py)
+
+YYMM=$(date '+%Y-%m')
+
+exec  >>${LOGDIR}/${BASE}_${YYMM}.log
+echo
+echo '>>>'
+echo Starting $SCRIPT at $(date)
+export
+
+if [ "$1" = 'restart' ]
+then
+    python3 -u $SCRIPT stop
+fi
+
+LOGFILE=$LOGDIR/${BASE}_daemon.log python3 -u $SCRIPT start
+
+echo Completed $SCRIPT at $(date)
+echo '<<<'
