37b291d3481b6f6bcea86b1b37ddda6cfe1a5515
Sebastian Bazley <sebb@apache.org>
2016-04-09 23:50:44 +0000

    No longer needed
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1738398 13f79535-47bb-0310-9956-ffa450edef68

--- restart_scandist.sh
+++
@@ -1,6 +0,0 @@
-# Restart scandist as a nohup process
-# TODO: this should really be integrated into system startup
-
-pkill -u root -f "scandist.py foreground"
-mv --backup=numbered nohup.out nohup.out.$(date -Idate)
-nohup python -u scandist.py foreground &
