05138d6ee593899668c5aa6efdf954c98e39e72d
Sebastian Bazley <sebb@apache.org>
2016-05-02 14:47:13 +0000

    Change sender; use constants
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1741986 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/sendmail.py
+++ scripts/sendmail.py
@@ -4,12 +4,18 @@ import smtplib
 # Import the email modules we'll need
 from email.mime.text import MIMEText
 
-def sendMail(subject, body='', recipients='Site Development <site-dev@apache.org>', sender='no-reply@reporter.apache.org', port=25):
+__SENDER__ = 'Reporter <apsite@apache.org>'
+__RECIPIENTS__ = 'Site Development <site-dev@apache.org>'
+__REPLY_TO__ = 'site-dev@apache.org'
+
+def sendMail(subject, body='', recipients=__RECIPIENTS__, sender=__SENDER__, port=25, replyTo=__REPLY_TO__):
     # Create a text/plain message
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = recipients
+    if replyTo != None:
+        msg['Reply-To'] = replyTo
     smtp = smtplib.SMTP('localhost', port)
     smtp.sendmail(sender, [recipients], msg.as_string())
     smtp.quit()
@@ -17,7 +23,7 @@ def sendMail(subject, body='', recipients='Site Development <site-dev@apache.org
 if __name__ == '__main__':
     # for testing locally:
     # sudo postfix start # MacoxX
-    # or start a debug server => need to change the SMTP port above
+    # or start a debug server => need to change the SMTP port
     # python -m smtpd -n -c DebuggingServer localhost:1025
     sendMail('Test message, please ignore', "Thanks!")
     print("Sent")
