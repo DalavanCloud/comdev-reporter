0baf087418b5ad90b144f36e5bc2c2606b4c63a9
Sebastian Bazley <sebb@apache.org>
2016-05-02 11:01:21 +0000

    Tweaks
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1741959 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/sendmail.py
+++ scripts/sendmail.py
@@ -4,7 +4,7 @@ import smtplib
 # Import the email modules we'll need
 from email.mime.text import MIMEText
 
-def sendMail(subject, body, recipients, sender='no-reply@reporter.apache.org', port=25):
+def sendMail(subject, body='', recipients='Site Development <site-dev@apache.org>', sender='no-reply@reporter.apache.org', port=25):
     # Create a text/plain message
     msg = MIMEText(body)
     msg['Subject'] = subject
@@ -19,5 +19,5 @@ if __name__ == '__main__':
     # sudo postfix start # MacoxX
     # or start a debug server => need to change the SMTP port above
     # python -m smtpd -n -c DebuggingServer localhost:1025
-    sendMail('Example SMTP message', "The quick brown fox ...", 'no-one')
+    sendMail('Test message, please ignore', "Thanks!")
     print("Sent")
