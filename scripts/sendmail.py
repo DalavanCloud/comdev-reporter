5e573017a9637e652fce3f377579ac84b46b043f
Sebastian Bazley <sebb@apache.org>
2016-06-21 14:56:58 +0000

    Allow code to work with a recipient list
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1749550 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/sendmail.py
+++ scripts/sendmail.py
@@ -13,17 +13,27 @@ def sendMail(subject, body='', recipients=__RECIPIENTS__, sender=__SENDER__, por
     msg = MIMEText(body)
     msg['Subject'] = subject
     msg['From'] = sender
-    msg['To'] = recipients
+    if isinstance(recipients, str):
+        msg['To'] = recipients
+    else:
+        msg['To'] = ",".join(recipients)
     if replyTo != None:
         msg['Reply-To'] = replyTo
     smtp = smtplib.SMTP('localhost', port)
-    smtp.sendmail(sender, [recipients], msg.as_string())
+#     smtp.set_debuglevel(True)
+    smtp.sendmail(sender, recipients, msg.as_string())
     smtp.quit()
 
 if __name__ == '__main__':
+    import sys
+    port = 25
+    if len(sys.argv) > 1: # argv[0] is the script name
+        port = int(sys.argv[1])
     # for testing locally:
     # sudo postfix start # MacoxX
     # or start a debug server => need to change the SMTP port
     # python -m smtpd -n -c DebuggingServer localhost:1025
-    sendMail('Test message, please ignore', "Thanks!")
+    sendMail('Test message, please ignore', "Thanks!", port=port)
     print("Sent")
+    sendMail('Another Test message, please ignore', "Thanks again!", recipients=['a.b.c','d.e.f'], port=port)
+    print("Sent second")
