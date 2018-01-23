e698da7ccb08dd7fc59f5c987957b1628100c5f3
Henk P. Penning <henkp@apache.org>
2018-01-23 09:32:47 +0000

    show checker errors, if any (21)
    git-svn-id: https://svn.apache.org/repos/asf/comdev/reporter.apache.org/trunk@1821973 13f79535-47bb-0310-9956-ffa450edef68

--- site/js/render-hppdev.js
+++ site/js/render-hppdev.js
@@ -840,7 +840,9 @@ function renderChecker(pmc) {
     for ( idx in data['summary'] ) {
       summ += ( '<li>' + data['summary'][idx] + "</li>\n" ) ;
     }
-    var ival = Date.now().getUnixTime() - meta['refreshed']['time'] ;
+    var date = new Date() ;
+    var time = date.getTime() ;
+    var ival = time - meta['refreshed']['time'] ;
     obj.innerHTML += "Site " + site + " checks the health of " + dist + " ;\n" ;
     obj.innerHTML += 'for ' + pmc + " it reports these errors :\n<ul>\n" + summ + "</ul>\n" ;
     obj.innerHTML += 'For details see ' + my_url(href,href) + "<br>\n" ;
